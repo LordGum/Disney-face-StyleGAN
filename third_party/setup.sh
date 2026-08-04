64960dd0f051282394bae680c40aa6305604bf8d
LordGum <dari.vision@mail.ru>
2026-08-04 11:35:02 +0300

    pipeline

--- third_party/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p third_party
-
-git clone https://github.com/omertov/encoder4editing.git third_party/encoder4editing
-
-git clone https://github.com/rosinality/stylegan2-pytorch.git third_party/stylegan2-pytorch
